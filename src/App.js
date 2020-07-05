41c886500ec3c89f2ca8b7b2fc69fbac5cc27a32
Haris-Taraholic <harisharry654@gmail.com>
2020-07-05 16:58:43 -0700

    Added MoreInfo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+
+import style from "./App.module.scss";
+
 import { Header } from "./components/Header/Header";
 import { Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { ListOfHeroes } from "./components/ListOfHeroes/ListOfHeroes";
+
 function App() {
   return (
-    <div>
+    <div className={style.wrapper}>
       <Header />
       <Container>
         <Route exact path="/" component={ListOfHeroes} />
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import style from "./Hero.module.scss";
 
@@ -9,11 +10,16 @@ import { HeroData } from "./HeroData/HeroData";
 export const Hero = ({ id, description, name, avatar }) => {
   return (
     <Row className={style.hero}>
-      <Col lg={3}>
+      <Col lg={2}>
         <Avatar url={avatar} />
       </Col>
-      <Col lg={7}>
+      <Col lg={6}>
         <HeroData name={name} description={description} />
+      </Col>
+      <Col lg={2} className="offset-2">
+        <Link to={`/hero/${id}`} className={style.showInfo}>
+          More
+        </Link>
       </Col>
     </Row>
   );
